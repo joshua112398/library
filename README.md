3b0a804088db8fa76de98f347311da81c24769dd
Josh Geronimo <joshuageronimo112398@gmail.com>
2022-05-31 02:09:16 -0700

    Add script functionality for dispaying book info

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
-# library
+# Library
+
+
+
+# Roadblocks
+- The book's information kept coming out as "undefined" when trying to generate it with JavaScript using the "this" keyword. This was solved by realizing that arrow functions do not provide a "this" of their own and instead take the "this" that was currently defined during time of its execution (in this case, I had it as undefined/global scope). Changing it from an arrow function to a regular anonymous function solved the issue.
--- scripts/script.js
+++ scripts/script.js
@@ -12,19 +12,61 @@ Book.prototype.info = () => {
     return `${title} by ${author}, ${pages} pages, ${readIndicator}`;
 }
 
-function addBookToLibrary() {
-    let book = new Book();
-    myLibrary.push(book);
+Book.prototype.display = function() {
+    const main = document.querySelector("main");
+    const newBook = document.createElement('div');
+    newBook.classList.add('book');
+    main.appendChild(newBook);
+
+    // Add book information
+    const name = document.createElement('h2')
+    name.textContent = `${this.title}`;
+    console.log(this.title);
+    name.classList.add('name');
+    newBook.appendChild(name);
+    const author = document.createElement('p')
+    author.textContent = `By ${this.author}`;
+    author.classList.add('author');
+    newBook.appendChild(author);
+    const pages = document.createElement('p')
+    pages.textContent = `${this.pages} pages`;
+    pages.classList.add('pages');
+    newBook.appendChild(pages);
+    let readIndicator = this.read ? "already read" : "not read yet";
+    const read = document.createElement('p')
+    read.textContent = `By ${this.author}`;
+    read.classList.add('read');
+    newBook.appendChild(read);
+
+    // Add the Delete and Read buttons
+    const buttons = document.createElement('div');
+    buttons.classList.add('book-buttons');
+    newBook.appendChild(buttons);
+    let readButton = document.createElement('button')
+    readButton.textContent = "Read";
+    let deleteButton = document.createElement('button')
+    deleteButton.textContent = "Delete";
+    buttons.appendChild(readButton);
+    buttons.appendChild(deleteButton);
+    return;
+
 }
 
-function displayBook(book) {
-    const main = document.querySelector("main");
-    const newBook = main.createElement('div');
-    newBook.classList.add('book');
+
+function addBookToLibrary(title, author, pages, read) {
+    let book = new Book(title, author, pages, read);
+    console.log("Book added");
+    myLibrary.push(book);
 }
 
 function displayLibrary() {
     for (let i = 0; i < myLibrary.length; i++) {
-        displayBook(myLibrary[i]);
+        myLibrary[i].display();
     }
+    return;
 }
+
+addBookToLibrary("Mango", "Josh Bullocks", 74, true);
+addBookToLibrary("Mango", "Josh Bullocks", 74, true);
+addBookToLibrary("Mango", "Josh Bullocks", 74, true);
+displayLibrary();
